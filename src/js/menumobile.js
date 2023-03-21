46cf32afe31fd6b0e06f43dc9d88851f33ae8b38
Leandro Geraldo <leandro.a.pd4@gmail.com>
2023-03-20 22:56:56 -0300

    adicionado download para currículo, adicionado imagens svg, adicionado favicon

--- src/js/menumobile.js
+++ src/js/menumobile.js
@@ -1,13 +1,13 @@
-function toggleMenu(event){
-    if (event.type === 'touchstart')event.preventDefault();
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
     const nav = document.querySelector(".nav")
     nav.classList.toggle("active");
     const active = nav.classList.contains('active');
     event.currentTarget.setAttribute('aria-expanded', active)
 
-    if(active){
+    if (active) {
         event.currentTarget.setAttribute('aria-label', 'Fechar Menu')
-    }else{
+    } else {
         event.currentTarget.setAttribute('aria-label', 'Abrir Menu')
     }
 }
@@ -16,3 +16,17 @@ const btnMobile = document.getElementById("btn-mobile")
 
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
+
+
+const habilidades = document.querySelectorAll('.habilidade')
+
+habilidades.forEach(habilidade=>{
+    habilidade.addEventListener('mouseenter',()=>{
+        const habilidadeAtiva = document.querySelector('.ativo')
+        habilidadeAtiva.classList.remove('ativo')
+        
+        habilidade.classList.add('ativo')
+
+
+    })
+})
